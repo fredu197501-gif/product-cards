2161fe478b558f35193709bb7c5a6f6e7ddd584e
Ulanbek <fredu197501@gmail.com>
2026-08-12 11:58:16 +0600

    Fixe errors

--- homework-10.js
+++ homework-10.js
@@ -7,7 +7,7 @@ const catalogList = document.getElementById("catalog-list");
 const catalogDescriptions = catalog.reduce((acc, product) => {
   acc[product.name] = product.description;
   return acc;
-}, []);
+}, {});
 
 // №3 Создал шаблон для продуктовых карточек.
 // №5 Функция, которая при запуске страницы выводит сообщение через функцию prompt "Сколько карточек отобразить?
@@ -33,7 +33,7 @@ function renderCards(arrayToRender) {
     catalogClone.querySelector(".catalog-subtitle").textContent = catalog.subtitle;
     catalogClone.querySelector(".catalog-description").textContent = catalog.description;
     catalogClone.querySelector(".catalog-composition .composition__value").textContent = catalog.composition.join(", ");
-    catalogClone.querySelector(".catalog-price").textContent = `Цена: ${catalog.price.toLocaleString("ru-RU")} ₽`;
+    catalogClone.querySelector(".catalog-price").innerHTML = `Цена: ${catalog.price.toLocaleString("ru-RU")} ₽`;
     catalogList.appendChild(catalogClone);
   });
 }
@@ -43,4 +43,4 @@ const cardsCount = getCountFromUser();
 const filteredCatalog = catalog.slice(0, cardsCount);
 renderCards(filteredCatalog);
 
-console.log(catalogList);
+console.log(catalogDescriptions);
